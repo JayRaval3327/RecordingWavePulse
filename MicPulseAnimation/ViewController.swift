60511d1f6cc0e93ee85227f8b24c08d4ab025575
Jay <jraval93@gmail.com>
2024-03-17 00:03:57 -0400

    Initial Commit

---
+++ MicPulseAnimation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MicPulseAnimation
+//
+//  Created by Jay Raval on 2024-03-17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
